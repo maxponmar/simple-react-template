706466ddbec95a79c556af640aa2516883e0d63d
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-01-03 16:50:16 -0700

    Footer Component Implented
    - Footer component accepts 3 props, title, name and email.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import styles from 'App.module.css';
-import Footer from 'components/layout/footer/Footer';
-import Header from 'components/layout/header/Header';
+import styles from "App.module.css";
+import Footer from "components/layout/footer/Footer";
+import Header from "components/layout/header/Header";
 
 function App() {
   return (
     <div className={styles.app}>
-      <Header title={"Simple React Template"}/>
-      <div className={styles.container}>
-
-      </div>
-      <Footer/>
+      <Header title={"Simple React Template"} />
+      <div className={styles.container}></div>
+      <Footer
+        title="Footer Title"
+        name="Your Name Here"
+        email="Your Email Here"
+      />
     </div>
   );
 }
--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,9 +1,23 @@
 import styles from "./Footer.module.css";
 
-const Footer = () => {
-  return(
-    <footer className={styles.footer}>Footer component</footer>
-  )
-}
+const Footer = ({ title, name, email }) => {
+  const getYear = () => new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.information}>
+        <span className={styles.copyright}>
+          © {getYear()} {name}
+        </span>
+        <span className={styles.contact}>
+          <a className={styles.link} href={`mailto:${email}`}>
+            {email}
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
