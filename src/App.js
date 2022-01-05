c2f8ea3a04cb668c9c41d16a4796aa25f644eb3a
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-01-04 19:27:09 -0700

    About component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import styles from "App.module.css";
+import styles from "./App.module.css";
 import Footer from "components/layout/footer/Footer";
 import Header from "components/layout/header/Header";
 import Sidebar from "components/layout/sidebar/Sidebar";
+import About from "containers/about/About";
+import Home from "containers/home/Home";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header title={"Simple React Template"} />
-      <Sidebar/>
-      <div className={styles.container}></div>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
       <Footer
         title="Footer Title"
         name="Your Name Here"
---
+++ src/containers/about/About.js
@@ -0,0 +1,11 @@
+import styles from "./About.module.css";
+
+function About() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>About Page</h2>
+    </div>
+  );
+}
+
+export default About;
