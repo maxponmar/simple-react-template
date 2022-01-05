f0711f187762adc615b7d30eb965b688aefd07c0
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-01-04 19:24:19 -0700

    Sidebar Links Added

--- src/components/layout/sidebar/Sidebar.js
+++ src/components/layout/sidebar/Sidebar.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import styles from "./Sidebar.module.css";
 import SidebarToggler from "./sidebartoggler/SidebarToggler";
+import { Link } from "react-router-dom";
+import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
 
 function Sidebar() {
   const [SidebarStatus, setSidebarStatus] = useState(true);
 
   const toggleSidebar = () => {
-    setSidebarStatus(oldState => !oldState);
+    setSidebarStatus((oldState) => !oldState);
   };
 
   return (
@@ -19,7 +21,13 @@ function Sidebar() {
             : `${styles.sidebar} ${styles.hidden}`
         }
       >
-        Sidebar
+        <h2 className={styles.title}>Sidebar</h2>
+        <Link className={styles.link} to="/">
+          <AiFillHome className={styles.icon} /> Home
+        </Link>
+        <Link className={styles.link} to="/about">
+          <AiFillInfoCircle className={styles.icon} /> About
+        </Link>
       </aside>
     </>
   );
