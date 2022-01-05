36738283b4ae29c1ef8a6704adcc2fb9cfedfdc1
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-01-04 19:21:13 -0700

    Home component created

---
+++ src/containers/home/Home.js
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Home Page</h2>
+    </div>
+  );
+}
+
+export default Home;
